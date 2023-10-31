7a124d989975e24698613c7d90647899c754a6a0
Ash3-23 <nerea.trebol@yahoo.es>
2023-10-31 14:50:28 +0100

    add-server-list

--- sala-virtual-caixabank/src/components/NavBar/NavBar.js
+++ sala-virtual-caixabank/src/components/NavBar/NavBar.js
@@ -3,35 +3,56 @@ import logo from '../../images/Logo-CaixaBank-1 1.png';
 import { Link } from 'react-router-dom';
 import './NavBar.css';
 import ActionButtons from '../ActionButtons/ActionButtons';
+import axios from 'axios'; // Asegúrate de que axios esté instalado
+
 
 const NavBar = () => {
   const [tiempoRestante, setTiempoRestante] = useState(300); // 5 minutos en segundos
   const tiempoTotal = 300; // 5 minutos en segundos
   const [titulo, setTitulo] = useState('Sala Virtual');
-  const [posicionEnCola, setPosicionEnCola] = useState(0);
+  const [posicionEnCola, setPosicionEnCola] = useState(1);
 
   const minutosRestantes = Math.floor(tiempoRestante / 60);
 
-  // Función para asignar la posición en la cola
-  const asignarPosicion = () => {
-    // Obtiene la última posición en la cola desde el almacenamiento local
-    const posicion = localStorage.getItem('posicionEnCola');
+  useEffect(() => {
+    const bienvenidaYaMostrada = localStorage.getItem('bienvenidaMostrada');
 
-    if (posicion) {
-      // Si ya hay una posición, incrementa en 1 la posición del nuevo cliente
-      setPosicionEnCola(Number(posicion) + 1);
+    if (!bienvenidaYaMostrada) {
+      setTitulo('Bienvenido!');
+      localStorage.setItem('bienvenidaMostrada', 'true');
+
+      // Realiza una solicitud POST al servidor para agregar el cliente a la cola
+      axios.post('http://localhost:4000/agregar-cliente', { nombre: 'NombreCliente' })
+        .then((response) => {
+          const nuevaPosicion = response.data.posicion;
+          setPosicionEnCola(nuevaPosicion);
+
+          // Guardar la posición en el LocalStorage
+          localStorage.setItem('posicionEnCola', nuevaPosicion);
+        })
+        .catch((error) => {
+          console.error('Error al obtener la posición:', error);
+        });
     } else {
-      // Si no hay posición previa, asigna 1 como posición inicial
-      setPosicionEnCola(1);
+      // Cargar la posición almacenada en el LocalStorage al cargar la página
+      const posicionAlmacenada = localStorage.getItem('posicionEnCola');
+      if (posicionAlmacenada) {
+        setPosicionEnCola(Number(posicionAlmacenada));
+      }
     }
-
-    // Almacena la nueva posición en el almacenamiento local
-    localStorage.setItem('posicionEnCola', posicionEnCola);
-  };
+  }, []);
 
   useEffect(() => {
-    asignarPosicion(); // Asigna la posición al cargar la página
-  }, []);
+    // Actualiza la posición cuando el tiempo de espera se agota
+    if (tiempoTotal <= 0) {
+      const posicion = localStorage.getItem('posicionEnCola');
+      if (posicion) {
+        const nuevaPosicion = Number(posicion) + 1;
+        setPosicionEnCola(nuevaPosicion);
+        localStorage.setItem('posicionEnCola', nuevaPosicion);
+      }
+    }
+  }, [tiempoTotal]);
 
   useEffect(() => {
     const barra = document.querySelector('.tiempo-espera-fill');
@@ -98,9 +119,11 @@ const NavBar = () => {
           <div className="tiempo-espera-fill"></div>
         </div>
       </div>
-      <p className='sala-p'>
-        Tu posición en la cola: {posicionEnCola}
-      </p>
+     
+        <p className="sala-p">
+          Tu posición en la cola: {posicionEnCola}
+        </p>
+
       <div className="sala-buttons2">
         <ActionButtons />
       </div>
