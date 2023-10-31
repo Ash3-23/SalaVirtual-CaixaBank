18f091c053b159914c504c6cf7b0a7394b0ffb69
Ash3-23 <nerea.trebol@yahoo.es>
2023-10-31 12:21:00 +0100

    localstorage

--- sala-virtual-caixabank/src/components/ActionButtons/ActionButtons.js
+++ sala-virtual-caixabank/src/components/ActionButtons/ActionButtons.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 import './ActionButtons.css'
 
 const ActionButtons = ({ text }) => {
   return (
     <div className="action-buttons">
       <button className="action-buttons-modificar">Cancelar</button>
+      <Link to='modificar-cita'>
         <button className="action-buttons-cancelar">Modificar</button>
+      </Link>
     </div>
   );
 };
--- sala-virtual-caixabank/src/components/NavBar/NavBar.js
+++ sala-virtual-caixabank/src/components/NavBar/NavBar.js
@@ -4,58 +4,46 @@ import { Link } from 'react-router-dom';
 import './NavBar.css';
 import ActionButtons from '../ActionButtons/ActionButtons';
 
-
 const NavBar = () => {
   const [tiempoRestante, setTiempoRestante] = useState(300); // 5 minutos en segundos
   const tiempoTotal = 300; // 5 minutos en segundos
-  const [titulo, setTitulo] = useState('Bienvenido!'); // Estado para el título
-  const [mostrarBienvenido, setMostrarBienvenido] = useState(true);
-  const [transicionTerminada, setTransicionTerminada] = useState(false);
+  const [titulo, setTitulo] = useState('Sala Virtual');
+  const [transicionTerminada, setTransicionTerminada] = useState(true);
 
-  const minutosRestantes = Math.floor(tiempoRestante / 60); // Convierte segundos a minutos
+  const minutosRestantes = Math.floor(tiempoRestante / 60);
 
   useEffect(() => {
     const barra = document.querySelector('.tiempo-espera-fill');
-  
+
     if (barra) {
       barra.style.width = `${((tiempoTotal - tiempoRestante) / tiempoTotal) * 100}%`;
     }
-  
+
     const interval = setInterval(() => {
       if (tiempoRestante > 0) {
         setTiempoRestante(tiempoRestante - 1);
-        if (mostrarBienvenido && tiempoTotal - tiempoRestante === 5) {
-          // Cambia a "Bienvenido!" después de 5 segundos
-          setMostrarBienvenido(false);
-  
-          // Simula la transición al cambiar el título después de un retraso
-          setTimeout(() => {
-            setTitulo('Sala Virtual');
-          }, 1000); // Cambia después de 1 segundo
-        }
       }
     }, 1000);
-  
+
     if (tiempoRestante <= 0) {
       // Aquí puedes agregar el código para mostrar el componente "suTurno"
       // por ejemplo, cambiando el estado o navegando a una nueva pantalla.
     }
-  
+
     return () => {
       clearInterval(interval);
     };
-  }, [tiempoRestante, mostrarBienvenido]);
-  
+  }, [tiempoRestante]);
 
   return (
     <div className="desktop-content">
       <Link to='/'>
         <img className="logo-caixabank" alt="Logo caixabank" src={logo} />
       </Link>
-     <div className="sala-details">
-     <div className={`sala-title ${transicionTerminada ? 'visible' : 'hidden'}`}>
-    {titulo}
-  </div>
+      <div className="sala-details">
+        <div className={`sala-title ${transicionTerminada ? 'visible' : 'hidden'}`}>
+          {titulo}
+        </div>
         <div className="sala-buttons">
           <Link to="/modificar-cita" className="boton-secondario-modificar">
             Modificar
@@ -67,7 +55,7 @@ const NavBar = () => {
         <div className="tiempo-espera-label">Tiempo de espera estimado</div>
         <div className="tiempo-espera-value">
           {minutosRestantes} min
-        </div> {/* Mostrar minutos y segundos */}
+        </div>
         <div className="tiempo-espera-barra">
           <div className="tiempo-espera-fill"></div>
         </div>
