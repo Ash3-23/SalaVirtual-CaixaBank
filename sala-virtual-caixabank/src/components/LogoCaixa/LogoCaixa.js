d431a5f9cbd849927b2dc636bc9724e9a7263b84
Ash3-23 <nerea.trebol@yahoo.es>
2023-11-01 11:21:35 +0100

    design-changes

--- sala-virtual-caixabank/src/components/LogoCaixa/LogoCaixa.js
+++ sala-virtual-caixabank/src/components/LogoCaixa/LogoCaixa.js
@@ -4,9 +4,7 @@ import logo from '../../images/Logo-CaixaBank-1 1.png';
 
 const LogoCaixa = () => {
   return (
-    <Link to='/'>
       <img className="logo-caixabank" alt="Logo caixabank" src={logo} />
-    </Link>
   );
 };
 
--- sala-virtual-caixabank/src/pages/ModificatedPage/ModificatedPage.js
+++ sala-virtual-caixabank/src/pages/ModificatedPage/ModificatedPage.js
@@ -1,12 +1,15 @@
 import React from 'react'
 // import NavBar from '../../components/NavBar/NavBar';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import LogoCaixa from '../../components/LogoCaixa/LogoCaixa'
 
 const ModificatedPage = () => {
   return (
     <div className="desktop-content">
-    <LogoCaixa />
+      <Link to="/">
+        <LogoCaixa />
+      </Link>
+
     </div>
   )
 }
