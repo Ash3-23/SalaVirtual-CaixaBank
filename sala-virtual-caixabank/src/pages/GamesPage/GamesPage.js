67a7dbe86999c32baef156c4be219977ff6b5e40
Ash3-23 <nerea.trebol@yahoo.es>
2023-11-02 15:30:48 +0100

    games

--- sala-virtual-caixabank/src/pages/GamesPage/GamesPage.js
+++ sala-virtual-caixabank/src/pages/GamesPage/GamesPage.js
@@ -16,29 +16,37 @@ const GamesPage = () => {
       </Link>
       <div className="information-frames">
         <div className="info-frame">
+        <a href="https://minijuegos.billetesargentinos.com.ar/juegos/ruleta/ruleta.htm" target="_blank" rel="noopener noreferrer" className="info-frame">
           <div className="info-frame-icon">
             <img src={ruleta} alt="info" />
           </div>
+        </a>
           <div className="info-frame-text">Ruleta</div>
         </div>
         <div className="info-frame">
+        <a href="https://srunners.com/aventuras/un-viaje-inesperado/personajes/" target="_blank" rel="noopener noreferrer" className="info-frame">
           <div className="info-frame-icon">
             <img src={escape} alt="info" />
           </div>
+        </a>
           <div className="info-frame-text">Escape Room</div>
         </div>
         <div className="info-frame">
+        <a href="https://www.cooljuegos.com/juego-en-linea/monopoly-online/" target="_blank" rel="noopener noreferrer" className="info-frame">
           <div className="info-frame-icon">
             <img src={monopoli} alt="info" />
           </div>
+        </a>
           <div className="info-frame-text">
             Monopoly
           </div>
         </div>
         <div className="info-frame">
+        <a href="https://www.cerebriti.com/juegos-de-matematicas/calculo-mental-rapido" target="_blank" rel="noopener noreferrer" className="info-frame">
           <div className="info-frame-icon">
             <img src={trivia} alt="info" />
           </div>
+        </a>
           <div className="info-frame-text">Trivia</div>
         </div>
       </div>
